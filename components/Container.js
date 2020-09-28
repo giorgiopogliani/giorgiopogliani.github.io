814a20a23d4079a64a90a8494d31c8279834267b
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2020-09-28 15:20:41 +0200

    wip

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,6 @@
 export default function Container({ children }){
     return (
-        <div className="py-12 w-full h-full max-w-6xl mx-auto overflow-auto">
+        <div className="py-12 w-full px-6 max-w-6xl mx-auto overflow-auto">
             {children}
         </div>
     )
