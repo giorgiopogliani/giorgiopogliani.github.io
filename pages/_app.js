a93d7ac683152a353214696fc4da80645d4f3f91
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2020-09-28 15:33:56 +0200

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
 import 'highlight.js/styles/atom-one-dark.css';
 
+import Router from 'next/router'
+
+import * as gtag from 'common/src/lib/gtag'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
