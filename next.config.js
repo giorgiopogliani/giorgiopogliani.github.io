8d7f4cd44b8768844fe8f35521a69c34eeab5123
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2020-09-28 13:37:14 +0200

    wip

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   target: 'serverless',
-  assetPrefix: !debug ? 'https://giorgiopogliani.me/' : '',
   webpack: function (config) {
     config.module.rules.push({ test: /\.md$/, use: 'raw-loader' })
     config.module.rules.push({ test: /\.yml$/, use: 'raw-loader' })
